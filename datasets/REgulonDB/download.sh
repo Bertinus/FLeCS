9edb0466e8c599e0a89870bc70530edc13a0d756
Joseph Viviano <joseph@viviano.ca>
2022-10-25 23:06:49 -0400

    gitignore

---
+++ datasets/REgulonDB/download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -c  http://regulondb.ccg.unam.mx/menu/download/datasets/files/network_tf_gene.txt
+wget -c http://regulondb.ccg.unam.mx/menu/download/datasets/files/network_tf_tf.txt
