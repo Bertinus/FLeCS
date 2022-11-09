cb5a1645229b7bcdfea816874fecced9e4e16b03
Joseph Viviano <joseph@viviano.ca>
2022-11-08 22:49:19 -0500

    added dataset

---
+++ datasets/STRING/download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Data for Homo Sapiens only
+# https://string-db.org/cgi/download?sessionId=bY5mHxmI1xAx&species_text=Homo+sapiens
+# Channels in data explained here
+# https://academic.oup.com/nar/article/47/D1/D607/5198476
+wget -c https://stringdb-static.org/download/protein.physical.links.detailed.v11.5/9606.protein.physical.links.detailed.v11.5.txt.gz
