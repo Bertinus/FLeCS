71e850baae23441357cd5b5ae39b36c124136045
Joseph Viviano <joseph@viviano.ca>
2022-12-02 21:45:46 -0500

    simple tests for interventions

--- tests/test_intervention.py
+++ tests/test_intervention.py
@@ -1,73 +1,35 @@
-import copy
-
 import pytest
 import torch
-from torch.distributions.gamma import Gamma
-from torch.distributions.normal import Normal
 
-from flecs.cell_population import CellPopulation, TestCellPop
-from flecs.intervention import Intervention
+from flecs.cell_population import TestCellPop
+from flecs.intervention import CrisprIntervention
+from flecs.trajectory import simulate_deterministic_trajectory
+from flecs.utils import set_seed
 
 
 @pytest.fixture
-def my_cells():
-    pass
+def cell_pop():
+    return TestCellPop()
 
 
-def test_intervention(my_cells):
-    my_ko_intervention = Intervention()
-    edges_before_intervention = copy.deepcopy(my_cells.edges)
+def test_inverventions(cell_pop):
+    set_seed(0)
+    intervention = CrisprIntervention(cell_pop)
 
-    # First KO
-    interv_gene_1 = int(edges_before_intervention[0, 0])
-    expected_number_of_removed_edges_1 = (my_cells.edges[:, 0] == interv_gene_1).sum()
-    my_ko_intervention.intervene(my_cells, interv_gene_1)
+    # Simulate trajectory for CRISPR.
+    cell_traj_0 = simulate_deterministic_trajectory(cell_pop, torch.linspace(0, 1, 100))
 
-    # Second KO
-    interv_gene_2 = int(my_cells.edges[0, 0])
-    expected_number_of_removed_edges_2 = (my_cells.edges[:, 0] == interv_gene_2).sum()
-    my_ko_intervention.intervene(my_cells, interv_gene_2)
+    intervention.intervene(gene=0)  # Intervene on gene 0
+    cell_pop.state = 10 * torch.ones(cell_pop.state.shape)  # Set initial state
 
-    assert (
-        len(my_cells.edges)
-        == len(edges_before_intervention)
-        - expected_number_of_removed_edges_1
-        - expected_number_of_removed_edges_2
-    )
+    # Simulate trajectory twice, with intervention reset between.
+    cell_traj_1 = simulate_deterministic_trajectory(cell_pop, torch.linspace(0, 1, 100))
 
-    my_ko_intervention.reset(my_cells)
+    intervention.reset()  # Reset intervention
+    cell_pop.state = 10 * torch.ones(cell_pop.state.shape)  # Set initial state
 
-    assert len(my_cells.edges) == len(edges_before_intervention)
+    cell_traj_2 = simulate_deterministic_trajectory(cell_pop, torch.linspace(0, 1, 100))
 
-
-def test_intervention_twice(my_cells):
-    my_ko_intervention = Intervention()
-    edges_before_intervention = copy.deepcopy(my_cells.edges)
-
-    # First KO
-    interv_gene_1 = int(edges_before_intervention[0, 0])
-    my_ko_intervention.intervene(my_cells, interv_gene_1)
-
-    with pytest.raises(ValueError):
-        my_ko_intervention.intervene(my_cells, interv_gene_1)
-
-
-def test_reset_drug_intervention(my_cells):
-
-    my_drug_intervention = DrugLinearIntervention()
-    direct_effects = torch.ones(10)
-
-    production_rates_before_intervention = my_cells.get_production_rates(my_cells.state)
-    my_drug_intervention.intervene(my_cells, direct_effects)
-    my_drug_intervention.reset(my_cells)
-
-    production_rates_after_intervention_and_reset = my_cells.get_production_rates(
-        my_cells.state
-    )
-
-    assert torch.isclose(
-        production_rates_before_intervention,
-        production_rates_after_intervention_and_reset,
-        rtol=1e-2,
-        atol=1e-2,
-    ).all()
+    assert torch.allclose(cell_traj_2[-1, ...], cell_traj_0[-1, ...])
+    assert not torch.allclose(cell_traj_2[-1, ...], cell_traj_1[-1, ...])
+    assert not torch.allclose(cell_traj_1[-1, ...], cell_traj_0[-1, ...])
