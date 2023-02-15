83d3882de4df7e4d5551a3672553e36481b97ff5
Joseph Viviano <joseph@viviano.ca>
2023-02-15 13:05:01 -0500

    removed setup file

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="flecs",
-    description="Flexible and Learnable Cell Simulator",
-    packages=find_packages(include=["flecs", "flecs.*"]),
-    version="0.1",
-)
